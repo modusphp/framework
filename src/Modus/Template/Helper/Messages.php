d72d15dbf6811eeabcfd5154ccbda13568304377
Brandon Savage <brandon@brandonsavage.net>
2014-03-03 22:56:53 -0500

    Adding messages sending to framework.

---
+++ src/Modus/Template/Helper/Messages.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Modus\Template\Helper;
+
+use Aura\View\Helper\AbstractHelper;
+use Aura\Session;
+
+
+class Messages extends AbstractHelper {
+
+    protected $segment;
+
+    public function __construct(Session\Segment $segment) {
+        $this->segment = $segment;
+    }
+
+    public function __invoke($url) {
+       $messages = $this->getErrors();
+       $messages .= $this->getMessages();
+       return $messages;
+    }
+
+    protected function getErrors() {
+        $message = $this->segment->getFlash('failure');
+        return '<div class="failure">' . $message . '</div>';
+
+    }
+
+    protected function getMessages() {
+        $message = $this->segment->getFlash('success');
+        return '<div class="success">' . $message . '</div>';
+    }
+}
