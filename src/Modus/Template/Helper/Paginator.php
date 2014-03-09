37933be6a68e6a7402bc9f819f493994165d8fd4
Brandon Savage <brandon@brandonsavage.net>
2014-03-08 21:54:10 -0500

    Adding pagination functionality.

---
+++ src/Modus/Template/Helper/Paginator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modus\Template\Helper;
+
+use Aura\View\Helper\AbstractHelper;
+use Pagerfanta\View;
+use Pagerfanta;
+
+class Messages extends AbstractHelper {
+
+    protected $defaultView;
+
+    public function __construct(View\ViewInterface $defaultView) {
+        $this->defaultView = $defaultView;
+    }
+
+    public function __invoke(Pagerfanta\PagerfantaInterface $pagerfanta, $route, array $options = array()) {
+        $routeGenerator = function($page) use ($route) { return $route . '/' . $page; };
+        return $this->defaultView->render($pagerfanta, $routeGenerator, $options);
+    }
+}
