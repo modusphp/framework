b0af86cda0ae09120245c101f95fe362ee144171
Brandon Savage <brandon@brandonsavage.net>
2014-03-20 18:11:04 -0400

    Adding Application back to the git repo.

---
+++ src/Application/Bootstrap.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Modus\Application;
+
+use Aura\Di;
+use Aura\Web;
+
+use Modus\Router;
+use Modus\Response\Manager as RespMgr;
+use Modus\ErrorLogging as Log;
+use Modus\Common\Controller\Exception;
+
+class Bootstrap {
+    
+    protected $config;
+    protected $di;
+    protected $router;
+    protected $responseMgr;
+    protected $errorHandler;
+    protected $context;
+
+    public function __construct(
+        $config,
+        Di\Container $di,
+        Web\Context $context,
+        Router\Standard $router,
+        RespMgr\Factory $responseMgr,
+        Log\Manager $handler
+    ) {
+        $this->di = $di;
+        $this->context = $context;
+        $this->router = $router;
+        $this->responseMgr = $responseMgr;
+        $this->errorHandler = $handler;
+
+        $this->config = $config;
+    }
+    
+    public function execute() {
+        $router = $this->router;
+        $routepath = $router->determineRouting($this->context->getServer());
+        if(!$routepath) {
+            throw new Exception\NotFound('The route "' . $router->getLastRoute() . '" was not found');
+        }
+
+        $route = $routepath->values;
+
+        $module = (isset($route['module'])) ? ucfirst($route['module']) : 'Application';
+        $controller = (isset($route['controller'])) ? ucfirst($route['controller']) : 'Index';
+        $action = (isset($route['action'])) ? $route['action'] : 'index';
+
+        $callable = "{$module}\\Controller\\{$controller}";
+
+        $params = $route;
+        unset($params['controller']);
+        unset($params['module']);
+        unset($params['action']);
+
+        $object = $this->di->newInstance($callable);
+        $response = $object->exec($action, $params);
+        $this->sendHttpResponse($response);
+    }
+    
+    protected function sendHttpResponse($controllerResponse) {
+        $httpManager = $this->responseMgr;
+        $responseMsg = $httpManager->getResponseMessage();
+
+        // If this is a redirect, let's do the redirect.
+        if($controllerResponse->isRedirect()) {
+            $responseMsg->headers->set('Location', $controllerResponse->getRedirect());
+            $responseMsg->setStatusCode($controllerResponse->getStatusCode());
+            $responseMsg->setStatusText($controllerResponse->getStatustext());
+            return $httpManager->send($responseMsg);
+        }
+        
+        foreach($controllerResponse->getHeaders() as $header => $value) {
+            $responseMsg->headers->set($header, $value);
+        }
+        
+        $responseMsg->setStatusCode($controllerResponse->getStatusCode());
+        $responseMsg->setStatusText($controllerResponse->getStatusText());
+        $responseMsg->setContent($controllerResponse->getContent());
+        $httpManager->send($responseMsg);
+    }
+}
