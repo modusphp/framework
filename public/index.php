be47b8ab779f4fa5ebe00bda526055a7a7a06bdf
Brandon Savage <brandon@brandonsavage.net>
2014-12-04 20:50:03 -0500

    Updating the index.php file.

--- public/index.php
+++ public/index.php
@@ -2,15 +2,12 @@
 
 require_once('../vendor/autoload.php');
 
-#$config = require_once('../config/config.php');
-#require_once('../config/services.php');
-
 $di = new Aura\Di\Container(new Aura\Di\Factory);
 $di->setAutoResolve(false);
 $config = new Modus\Config\Config($_SERVER['MY_ENV'], realpath('../config'), $di);
 
 $di->set('config', $config);
 
+
 $framework = $di->newInstance('Modus\Application\Bootstrap');
 $framework->execute();
-
