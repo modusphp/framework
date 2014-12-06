2e0d318ff3ddc6ba5258ca03a5c8d42474799bf6
Brandon Savage <brandon@brandonsavage.net>
2014-12-06 12:23:29 -0500

    Improving test setup, adding a test to improve coverage.

--- tests/src/BaseUserTest.php
+++ tests/src/BaseUserTest.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once '../vendor/autoload.php';
-
 class BaseUserTest extends PHPUnit_Framework_TestCase {
 
     protected $userObj;
--- tests/src/MessagesHelperTest.php
+++ tests/src/MessagesHelperTest.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once '../vendor/autoload.php';
-
 class MessagesHelperTest extends PHPUnit_Framework_TestCase {
 
     public function testMessagesProcessCorrectly() {
@@ -18,6 +16,24 @@ class MessagesHelperTest extends PHPUnit_Framework_TestCase {
         $values = $messages();
 
         $shouldGet = '<div class="failure">def</div><div class="success">abc</div>';
+
+        $this->assertEquals($values, $shouldGet);
+    }
+
+    public function testNoMessagesIsNull() {
+
+        $segment = Mockery::mock('Aura\Session\Segment');
+        $segment->shouldReceive('getFlash')->withArgs(['success'])->andReturn('');
+        $segment->shouldReceive('getFlash')->withArgs(['failure'])->andReturn('');
+
+        $m = Mockery::mock('Aura\Session\Manager');
+        $m->shouldReceive('newSegment')->once()->andReturn($segment);
+        $session = new Modus\Session\Aura($m);
+
+        $messages = new Modus\Template\Helper\Messages($session);
+        $values = $messages();
+
+        $shouldGet = null;
 
         $this->assertEquals($values, $shouldGet);
     }
