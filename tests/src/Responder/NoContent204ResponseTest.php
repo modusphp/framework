494902ad434f63ce6f6cdb501be642114ff8f06b
Brandon Savage <brandon@brandonsavage.net>
2014-12-12 16:10:50 -0500

    Adding a 204 response code test.

---
+++ tests/src/Responder/NoContent204ResponseTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class NoContent204ResponseTest extends PHPUnit_Framework_TestCase {
+
+    public function test204StatusCodeSetOnResponse() {
+        $response =new Aura\Web\WebFactory($GLOBALS);
+        $response = $response->newResponse();
+        $view = Mockery::mock('Aura\View\View');
+
+        $acceptFactory = new Aura\Accept\AcceptFactory();
+        $accept = $acceptFactory->newInstance();
+
+        $noContent = new Modus\Responder\NoContent204Response($response, $view, $accept);
+        $noContent->process([]);
+
+        $this->assertEquals('HTTP/1.1 204 No Content', $response->status->get());
+    }
+
+}
