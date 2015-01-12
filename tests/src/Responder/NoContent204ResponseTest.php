3e540f9e7040802aad8718a007fa53ba5ff14b71
Brandon Savage <brandon@brandonsavage.net>
2015-01-12 13:06:51 -0500

    Last commit didn't also fix the test that relies on the constructor signature.

--- tests/src/Responder/NoContent204ResponseTest.php
+++ tests/src/Responder/NoContent204ResponseTest.php
@@ -10,7 +10,7 @@ class NoContent204ResponseTest extends PHPUnit_Framework_TestCase {
         $acceptFactory = new Aura\Accept\AcceptFactory();
         $accept = $acceptFactory->newInstance();
 
-        $noContent = new Modus\Responder\NoContent204Response($response, $view, $accept);
+        $noContent = new Modus\Responder\NoContent204Response($response, $view, $accept, new \Aura\Html\HelperLocator());
         $noContent->process([]);
 
         $this->assertEquals('HTTP/1.1 204 No Content', $response->status->get());
