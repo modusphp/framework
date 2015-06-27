b2b8d64a019ab3dee4d1388fc7a1c03def446ea0
Brandon Savage <brandon@brandonsavage.net>
2015-06-27 12:19:44 -0400

    Adding a basic content type class.

---
+++ src/Response/ContentTypes.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modus\Response;
+
+abstract class ContentTypes
+{
+    const APPLICATION_JSON = 'application/json';
+    const APPLICATION_XML = 'appplication/xml';
+    const APPLICATION_PDF = 'application/pdf';
+
+    const TEXT_HTML = 'text/html';
+    const TEXT_PLAIN = 'text/plain';
+    const TEXT_XML = 'text/xml';
+}
