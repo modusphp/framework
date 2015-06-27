61a9fb3491f245176bc12e077537a4e54b6b9e37
Brandon Savage <brandon@brandonsavage.net>
2015-06-27 13:09:29 -0400

    Adding more of the common content types.

--- src/Response/ContentTypes.php
+++ src/Response/ContentTypes.php
@@ -10,7 +10,31 @@ abstract class ContentTypes
     const APPLICATION_XML = 'appplication/xml';
     const APPLICATION_PDF = 'application/pdf';
 
+    const IMAGE_GIF = 'image/gif';
+    const IMAGE_PNG = 'image/png';
+    const IMAGE_PJPEG = 'image/pjpeg';
+    const IMAGE_PNG = 'image/png';
+    const IMAGE_BMP = 'image/bmp';
+    const IMAGE_SVGXML = 'image/svg+xml';
+    const IMAGE_TIFF = 'image/tiff';
+
+    const TEXT_CSS = 'text/css';
+    const TEXT_CSV = 'text/csv';
     const TEXT_HTML = 'text/html';
+    const TEXT_MARKDOWN = 'text/markdown';
+    const TEXT_JAVASCRIPT = 'text/javascript';
     const TEXT_PLAIN = 'text/plain';
+    const TEXT_RTF = 'text/rtf';
+    const TEXT_VCARD = 'text/vcard';
     const TEXT_XML = 'text/xml';
+
+    const VIDEO_AVI = 'video/avi';
+    const VIDEO_MPEG = 'video/mpeg';
+    const VIDEO_MP4 = 'video/mp4';
+    const VIDEO_OGG = 'video/ogg';
+    const VIDEO_QUICKTIME = 'video/qicktime';
+    const VIDEO_WEBM = 'video/webm';
+    const VIDEO_MATROSKA = 'video/matroska';
+    const VIDEO_WINDOWS = 'video/x-ms-wmv';
+    const VIDEO_FLASH = 'video/x-flv';
 }
