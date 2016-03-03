f412175b438c6dc04f707c0d243ae6b40f54fb31
Brandon Savage <brandon@brandonsavage.net>
2016-03-03 09:19:41 -0500

    Adding helpers to default HTML responder.

--- src/Response/Abstracts/HtmlOnlyResponder.php
+++ src/Response/Abstracts/HtmlOnlyResponder.php
@@ -2,6 +2,7 @@
 
 namespace Modus\Response\Abstracts;
 
+use Aura\Html\HelperLocator;
 use Aura\View\View;
 use Modus\Payload\PayloadInterface;
 use Modus\Response\Interfaces\HtmlGenerator;
@@ -9,12 +10,18 @@ use Modus\Response\Response;
 
 abstract class HtmlOnlyResponder implements HtmlGenerator
 {
+    protected $response;
+    protected $template;
+    protected $helpers;
+
     public function __construct(
         Response $response,
-        View $template
+        View $template,
+        HelperLocator $helpers
     ) {
         $this->response = $response;
         $this->template = $template;
+        $this->helpers = $helpers;
     }
 
     public function checkContentResponseType()
