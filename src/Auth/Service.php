cfab64ed3158a20266634ffcc31591fba46d57b2
Brandon Savage <brandon@brandonsavage.net>
2018-12-13 09:29:12 -0500

    Updating a small bit of the code for clarity that it's AuthException, not \Exception.

--- src/Auth/Service.php
+++ src/Auth/Service.php
@@ -4,7 +4,7 @@ namespace Modus\Auth;
 
 use Aura\Auth;
 use Aura\Auth\Service as AuthService;
-use Aura\Auth\Exception;
+use Aura\Auth\Exception as AuthException;
 
 class Service
 {
@@ -32,7 +32,7 @@ class Service
     {
         try {
             $this->resumeService->resume($this->userObj);
-        } catch (Exception $e) {
+        } catch (AuthException $e) {
             $this->handleException($e);
         }
         return $this->getUser();
@@ -42,7 +42,7 @@ class Service
     {
         try {
             $this->loginService->login($this->userObj, ['username' => $user, 'password' => $pass]);
-        } catch (Exception $e) {
+        } catch (AuthException $e) {
             $this->handleException($e);
         }
         return $this->getUser();
@@ -52,60 +52,60 @@ class Service
     {
         try {
             $this->logoutService->logout($this->userObj);
-        } catch (Exception $e) {
+        } catch (AuthException $e) {
             $this->handleException($e);
         }
         return $this->getUser();
     }
 
-    protected function handleException(Exception $e)
+    protected function handleException(AuthException $e)
     {
         switch (true) {
-            case ($e instanceof Exception\AlgorithmNotAvailable):
+            case ($e instanceof AuthException\AlgorithmNotAvailable):
                 $this->setError('The algorithm specified was unavailable', $e->getMessage());
                 break;
 
-            case ($e instanceof Exception\BindFailed):
+            case ($e instanceof AuthException\BindFailed):
                 $this->setError('LDAP Binding failed.', $e->getMessage());
                 break;
 
-            case ($e instanceof Exception\ConnectionFailed):
+            case ($e instanceof AuthException\ConnectionFailed):
                 $this->setError('The LDAP/IMAP connection failed.', $e->getMessage());
                 break;
 
-            case ($e instanceof Exception\FileNotFound):
+            case ($e instanceof AuthException\FileNotFound):
                 $this->setError('The password file could not be found.', $e->getMessage());
                 break;
 
-            case ($e instanceof Exception\FileNotReadable):
+            case ($e instanceof AuthException\FileNotReadable):
                 $this->setError('The file specified could not be read.', $e->getMessage());
                 break;
 
-            case ($e instanceof Exception\MultipleMatches):
+            case ($e instanceof AuthException\MultipleMatches):
                 $this->setError('There were multiple matches for your credentials in the database.');
                 break;
 
-            case ($e instanceof Exception\PasswordColumnNotSpecified):
+            case ($e instanceof AuthException\PasswordColumnNotSpecified):
                 $this->setError('The password column was not specified in the database.');
                 break;
 
-            case ($e instanceof Exception\PasswordIncorrect):
+            case ($e instanceof AuthException\PasswordIncorrect):
                 $this->setError('The password was incorrect.');
                 break;
 
-            case ($e instanceof Exception\PasswordMissing):
+            case ($e instanceof AuthException\PasswordMissing):
                 $this->setError('The password was not provided.');
                 break;
 
-            case ($e instanceof Exception\UsernameColumnNotSpecified):
+            case ($e instanceof AuthException\UsernameColumnNotSpecified):
                 $this->setError('The username column was not specified in the database.');
                 break;
 
-            case ($e instanceof Exception\UsernameMissing):
+            case ($e instanceof AuthException\UsernameMissing):
                 $this->setError('The username was not provided.');
                 break;
 
-            case ($e instanceof Exception\UsernameNotFound):
+            case ($e instanceof AuthException\UsernameNotFound):
                 $this->setError('The username was not found.');
                 break;
 
